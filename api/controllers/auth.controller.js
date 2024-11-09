4b5815c7d5d33d382307e76f228e9753df6870da
jajangscript <jajangkdi007@gmail.com>
2024-11-09 20:41:58 +0800

    create sign in api route
    Signed-off-by: jajangscript <jajangkdi007@gmail.com>

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,17 +1,48 @@
-import User from './../models/user.model.js';
-import bcryptjs from 'bcryptjs';
+import User from "./../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
-export const signup = async (req,res,next)=>{
-    const{username, email,password}=req.body;
-    const hashedPassword = bcryptjs.hashSync(password, 10);
-    const newUser = new User({username, email, password: hashedPassword});
-    
-try{
- await newUser.save()
-    res.status(201).json({message:"User created has successfuly"});
-}catch (error){
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+
+  try {
+    await newUser.save();
+    res.status(201).json({ message: "User created has successfuly" });
+  } catch (error) {
     next(error);
-}
+  }
+};
 
-   
+// signin controller function
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    // Find user by email
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+
+    // Compare the provided password with the stored hashed password
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong credentials"));
+
+    // Generate JWT token
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...rest } = validUser._doc;
+    const expiryDate = new Date(Date.now() + 3600000); // 1 Jam
+    res
+      .cookie("acces_token", token, {
+        expires: expiryDate,
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "Strict",
+      })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
